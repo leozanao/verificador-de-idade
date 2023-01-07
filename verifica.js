ea2b00269af1631079e6a532fbe05eec596784b3
Leonardo Zanão <120062418+leozanao@users.noreply.github.com>
2023-01-07 16:07:16 -0300

    adicionei imagens ao site

--- verifica.js
+++ verifica.js
@@ -8,12 +8,35 @@ function verificar(){
     }else{
         var fsex = document.getElementsByName('sex')
         var idade = ano - Number(fano.value)
+        var img = document.createElement('img')
+        img.setAttribute('id', 'img')
         res.innerHTML = `Sua idade é ${idade}`
         if (fsex[0].checked){
             genero = 'Homem'
+            if(idade>0 && idade<=5){
+                img.setAttribute('src', 'imagens/bebe-menino.png')
+
+            }
+            else if(idade<16){
+                img.setAttribute('src', 'imagens/jovem-boy.png')
+            }else{
+                img.setAttribute('src', 'imagens/homem.png')
+            }
+
         }else {
             genero = 'Mulher'
+            if(idade>0 && idade<=5){
+                img.setAttribute('src', 'imagens/bebe-menina.png')
+
+            }
+            else if(idade<16){
+                img.setAttribute('src', 'imagens/jovem-girl.png')
+            }else{
+                img.setAttribute('src', 'imagens/mulher.jpg')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Você é ${genero} e sua idade é ${idade} anos.`
+        res.appendChild(img)
     }
 }
